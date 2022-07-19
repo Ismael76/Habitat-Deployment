bd06f773c3f45c3eedd9f85c154ee66b6bef1c3a
Ismael Ali <ismaelali78@outlook.com>
2022-07-19 01:01:00 +0100

    Added a connection to our pg database on heroku to server

--- dbconfig/init.js
+++ dbconfig/init.js
@@ -1,3 +1,17 @@
-// const { Pool } = require("pg");
-// const pool = new Pool();
-// module.exports = pool;
+const { Pool } = require("pg");
+
+//Connect PG Database We Created On Heroku To Our Server
+let config;
+
+if (process.env.DATABASE_URL) {
+  config = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
+
+const pool = new Pool(config);
+
+module.exports = pool;
